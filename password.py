354b4ca3deb83253552aa613e55fe172b779c8ed
enock24 <enookimuu@gmail.com>
2020-11-21 11:30:30 +0300

    add user function

--- password.py
+++ password.py
@@ -9,3 +9,12 @@ class User:
         self.name = name
         self.username = username
         self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)    
+
--- password_test.py
+++ password_test.py
@@ -7,4 +7,21 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Langat", "justo01", "justo1234")
+        self.new_user = User("Towett", "enock24", "enock2470")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.name, "Towett")
+        self.assertEqual(self.new_user.username, "enock24")
+        self.assertEqual(self.new_user.password, "enock2470")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+        the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
