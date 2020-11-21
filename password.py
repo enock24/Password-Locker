16bfbc7bd80b220e35f678c28289777015da3822
enock24 <enookimuu@gmail.com>
2020-11-21 16:36:05 +0300

    add classdecorator for credential_exsist

--- password.py
+++ password.py
@@ -97,3 +97,18 @@ class Credential:
             if credential.username == username:
                 return credential  
 
+    @classmethod
+    def credential_exist(cls,name):
+        '''
+        Method that checks if a credential exists from the credential list.
+        Args:
+            name: name to search if it exists
+        Returns :
+            Boolean: True or false depending if the credential exists
+        '''
+        for credential in cls.credential_list:
+            if credential.name == name:
+                    return True
+
+        return False     
+
